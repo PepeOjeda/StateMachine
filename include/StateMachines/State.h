d60fbfa2ac9d046273f86ebfa7f46c1a731570be
Pepe Ojeda <ojedamorala@hotmail.com>
2025-03-20 11:14:05 +0100

    made OnEnterState and OnExitState protected, and removed OnUpdate

--- include/StateMachines/State.h
+++ include/StateMachines/State.h
@@ -1,10 +1,14 @@
 #pragma once
-namespace StateMachines{
+namespace StateMachines
+{
+    // forward declaration for friend statement
+    template <class T>
+    class StateMachine;
 
     /*this class is templated so you can control the type of the arguments that are passed to the functions
     Almost always, the way it is supposed to be used is:
-        
-    class My_State : public State<My_State> 
+
+    class My_State : public State<My_State>
     {};
 
     ...
@@ -15,21 +19,31 @@ namespace StateMachines{
         ...
     };
 
-    That way, inside of (for example) CanExitState(next), you know that the next state is not just any State, but specifically My_State, and can access its members accordingly.
+    That way, inside of (for example) CanExitState(next), you know that the next state is not just any State, but specifically My_State,
+    and can access its members accordingly.
     */
 
     template <class T>
     class State
     {
+        friend class StateMachine<T>;
+
     public:
         State() = default;
         State(const State<T>& other) = default;
         ~State() = default;
 
-        virtual bool CanEnterState(const T* previousState) const {return true;}
-        virtual bool CanExitState(const T* nextState) const {return true;}
+        virtual bool CanEnterState(const T* previousState) const
+        {
+            return true;
+        }
+        virtual bool CanExitState(const T* nextState) const
+        {
+            return true;
+        }
+
+    protected:
         virtual void OnEnterState(T* previousState) = 0;
         virtual void OnExitState(T* nextState) = 0;
-        virtual void OnUpdate() = 0;
     };
-}
+} // namespace StateMachines
--- include/StateMachines/StateMachine.h
+++ include/StateMachines/StateMachine.h
@@ -4,24 +4,9 @@
 
 namespace StateMachines
 {
-    template<class T>
+    template <class T>
     class StateMachine
     {
-    private:
-        T* currentState;
-        bool canChange(const T* next)
-        {
-            bool canDo = true;
-            if(currentState != nullptr)
-                canDo = canDo && currentState->CanExitState(next);
-            
-            if(next != nullptr)
-                canDo = canDo && next->CanEnterState(currentState);
-            else
-                canDo = false;
-
-            return canDo;
-        }
     public:
         StateMachine()
         {
@@ -29,34 +14,59 @@ namespace StateMachines
             currentState = nullptr;
         }
 
-        //sets currentState = next if both current->CanExitState() and next->CanEnterState() return true. If current == next, nothing happens
+        // sets currentState = next if both current->CanExitState() and next->CanEnterState() return true. If current == next, nothing happens
         void trySetState(T* next)
         {
-            if(canChange(next))      
+            if (canChange(next))
                 forceSetState(next);
         }
-        //same as trySetState, but OnExitState and OnEnterState will trigger even if current == next
+        // same as trySetState, but OnExitState and OnEnterState will trigger even if current == next
         void tryResetState(T* next)
         {
-            if(canChange(next))
+            if (canChange(next))
                 forceResetState(next);
         }
-        //same as trySetState, but does not check canEnter or canExit 
+        // same as trySetState, but does not check canEnter or canExit
         void forceSetState(T* next)
         {
-            if(currentState != next)
+            if (currentState != next)
                 forceResetState(next);
         }
-        //same as tryResetState, but does not check canEnter or canExit  
+        // same as tryResetState, but does not check canEnter or canExit
         void forceResetState(T* next)
         {
-            if(currentState != nullptr)
-                currentState->OnExitState(next);
+            if (currentState != nullptr)
+                AsBase(currentState)->OnExitState(next);
             T* previous = currentState;
             currentState = next;
-            currentState->OnEnterState(previous);
+            AsBase(currentState)->OnEnterState(previous);
         }
-        T* getCurrentState() const{return currentState;}
+        T* getCurrentState() const
+        {
+            return currentState;
+        }
+
+    private:
+        T* currentState;
+        bool canChange(const T* next)
+        {
+            bool canDo = true;
+            if (currentState != nullptr)
+                canDo = canDo && currentState->CanExitState(next);
+
+            if (next != nullptr)
+                canDo = canDo && next->CanEnterState(currentState);
+            else
+                canDo = false;
+
+            return canDo;
+        }
+
+        // required for the friend declaration to take effect, so this class can call OnEnter and OnExit
+        State<T>* AsBase(T* ptr)
+        {
+            return dynamic_cast<State<T>*>(ptr);
+        }
     };
 
-}
+} // namespace StateMachines
